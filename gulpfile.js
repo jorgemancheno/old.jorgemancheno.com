e0df94fc40dd141a2367b3b70f4bcc6d7a0bf351
Jorge Mancheno <git@jorgemancheno.com>
2018-01-30 13:39:21 -0500

    More cleanup.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
+// Sass
 gulp.task('sass:build', function () {
   return gulp.src('./src/css/sass/site.sass')
     .pipe(plumber(function(error) {
@@ -21,6 +22,8 @@ gulp.task('sass:watch', function () {
   gulp.watch('./src/css/sass/*.sass', watchSass);
 });
 
+
+// JS
 gulp.task('js:build', function() {
   return gulp.src([
       './node_modules/jquery/dist/jquery.min.js',
@@ -41,4 +44,17 @@ gulp.task('js:watch', function() {
   gulp.watch('./src/js/*.js', watchJS);
 });
 
-gulp.task('serve', gulp.series('sass:build', 'js:build', gulp.parallel('sass:watch', 'js:watch')));
+
+// Images
+gulp.task('images:build', function () {
+  return gulp.src('./src/images/**/*')
+    .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('images:watch', function() {
+  const watchImages = gulp.series(['images:build'])
+  gulp.watch('./src/images/**/*', watchImages);
+});
+
+
+gulp.task('serve', gulp.series('sass:build', 'js:build', 'images:build', gulp.parallel('sass:watch', 'js:watch', 'images:watch')));
